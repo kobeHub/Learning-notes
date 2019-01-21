55d40f59d4da814c1fea4a37f0d265a1432353b4
kobe <jdgets111@gmail.com>
2019-01-21 20:49:49 +0800

    Add basic interface test

--- GoInfo/gocode/src/github.com/nf/hello/find_vowels.go
+++ GoInfo/gocode/src/github.com/nf/hello/find_vowels.go
@@ -30,7 +30,9 @@ func (s myString) FindVowels() []rune {
 
 func main() {
   var s myString = "Just a test, and there is vowels, BELIEVE U."
-  vowels := s.FindVowels()
+  var v VowelsFinder = s
+  vowels := s.FindVowels()   // 定义了基于myString类型的method，所以可以通过该变量调用
   fmt.Println(s)
   fmt.Printf("%c ", vowels)
+  fmt.Printf("% c", v.FindVowels())    // 也可以使用基于接口的对象调用，所以并不完全符合OOP
 }
