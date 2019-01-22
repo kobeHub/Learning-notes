ad80ae5446dd201a24b796a14794526ec4276927
kobe <jdgets111@gmail.com>
2019-01-22 22:00:37 +0800

    Implement multi interface

--- GoInfo/gocode/src/github.com/nf/hello/salary_cul.go
+++ GoInfo/gocode/src/github.com/nf/hello/salary_cul.go
@@ -8,6 +8,19 @@ import (
 
 type SalaryCalculator interface {
   CalculateSalary() int
+  Display()
+}
+
+type SalaryChanger interface {
+  IncreaseBaisc(ins int)
+  DescreaseBasic(des int)
+}
+
+
+// 实现了多个接口，为了可以调用所有接口中的方法，需要定义一个总的接口
+type SalaryOptions interface {
+  SalaryCalculator
+  SalaryChanger
 }
 
 type Permanent struct {
@@ -26,9 +39,37 @@ func (p Permanent) CalculateSalary() int {
   return p.basicpay + p.pf
 }
 
+func (p Permanent) Display() {
+  fmt.Printf("Permanent:%s salary:%d\n", p.empId, p.basicpay + p.pf)
+}
+
+func (p *Permanent) IncreaseBaisc(ins int) {
+  fmt.Printf("Now increase %s salry %d\n", p.empId, ins)
+  p.basicpay += ins
+}
+
+func (p *Permanent) DescreaseBasic(des int) {
+  fmt.Printf("Now descrease %s salry %d\n", p.empId, des)
+  p.basicpay -= des
+}
+
 // salary of contract is basic pay
 func (c Contract) CalculateSalary() int {
   return c.basicpay
+}
+
+func (c Contract) Display() {
+  fmt.Println("Contract:%s salary:%d\n", c.empId, c.basicpay)
+}
+
+func (c *Contract) IncreaseBaisc(ins int) {
+  fmt.Printf("Now increase %s salry %d\n", c.empId, ins)
+  c.basicpay += ins
+}
+
+func (c *Contract) DescreaseBasic(des int) {
+  fmt.Printf("Now descrease %s salry %d\n", c.empId, des)
+  c.basicpay -= des
 }
 
 // calculate total employees' salary
@@ -60,4 +101,14 @@ func main() {
   fmt.Println("All the employees")
   fmt.Println(emps)
   fmt.Println("Total salary:", expense)
+
+  var emp SalaryOptions = &p1    // 此处必须使用地址进行赋值
+  // 因为需要更改对于调用者可见，所以method只可以使用指针接收器
+  // 而实现该接口时使用指针接收器，所以实现该接口的是指针类型
+  // 只有指针类型才可以进行赋值，然后再调用其他操作
+  emp.DescreaseBasic(100)
+  emp.Display()    // 接收器为value，可以使用指针类型的接口进行调用，编译器自动解引用
+  emp.IncreaseBaisc(1000)
+  emp.Display()
+
 }
