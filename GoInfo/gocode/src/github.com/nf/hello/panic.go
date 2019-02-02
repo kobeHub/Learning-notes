e23625e41945bae5209f8aa048d45a1cccae952a
kobe <jdgets111@gmail.com>
2019-02-02 11:29:54 +0800

    Add the panic use case Sat Feb  2 11:29:53 CST 2019

--- GoInfo/gocode/src/github.com/nf/hello/panic.go
+++ GoInfo/gocode/src/github.com/nf/hello/panic.go
@@ -2,11 +2,38 @@ package main
 
 import (
   "fmt"
+  "time"
 )
 
 
+// function to recover from main goroutines
+func recoverName() {
+  if r := recover(); r != nil {
+    fmt.Println("recover from", r)
+  }
+}
+
+func recovery() {
+  if r := recover(); r != nil {
+    fmt.Println("recovered from", r)
+  }
+}
+
+// another goroutine recover
+func a() {
+  defer recovery()
+  fmt.Println("Inside A")
+  go b()
+  time.Sleep(1 * time.Second)
+}
+
+func b() {
+  fmt.Println("Inside B")
+  panic("Panic from B")
+}
+
 func fullName(firstName, lastName *string) {
-  defer fmt.Println("defered call in the fullName")
+  defer recoverName()
   if firstName == nil {
     panic("runtime error: the firstName is nil")
   }
@@ -21,5 +48,9 @@ func main() {
   name := "Hordan"
   defer fmt.Println("defered call from main routine")
   fullName(&name, nil)
+  fmt.Println("normally returned from main")
+
+  fmt.Println()
+  a()
   fmt.Println("normally returned from main")
 }
