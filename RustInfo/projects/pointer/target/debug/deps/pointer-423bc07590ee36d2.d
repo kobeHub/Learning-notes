09ae6d64386687134a727fe2a5d8b1e3d54fae4d
Inno Jia <jdgets111@gmail.com>
2019-11-18 16:27:44 +0800

    fresh Mon 18 Nov 2019 04:27:44 PM CST

---
+++ RustInfo/projects/pointer/target/debug/deps/pointer-423bc07590ee36d2.d
@@ -0,0 +1,5 @@
+/home/inno/Learning-notes/RustInfo/projects/pointer/target/debug/deps/pointer-423bc07590ee36d2: src/lib.rs
+
+/home/inno/Learning-notes/RustInfo/projects/pointer/target/debug/deps/pointer-423bc07590ee36d2.d: src/lib.rs
+
+src/lib.rs:
---
+++ RustInfo/projects/pointer/target/debug/pointer-423bc07590ee36d2.d
@@ -0,0 +1,1 @@
+/home/inno/Learning-notes/RustInfo/projects/pointer/target/debug/pointer-423bc07590ee36d2: /home/inno/Learning-notes/RustInfo/projects/pointer/src/lib.rs
