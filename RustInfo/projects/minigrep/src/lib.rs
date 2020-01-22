648899ae6b67298cedd0b9e95197926331e077ac
Inno Jia <jdgets111@gmail.com>
2020-01-22 15:44:51 +0800

    review test Wed 22 Jan 2020 03:44:51 PM CST

--- RustInfo/projects/minigrep/src/lib.rs
+++ RustInfo/projects/minigrep/src/lib.rs
@@ -23,7 +23,7 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        // Get from environment 
+        // Get from environment
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {query, filename, case_sensitive})
     }
@@ -52,21 +52,21 @@ impl Config {
 //}
 
 // Extract the main logic from the main function
-// Use the Box<dyn Error> trait object to get a 
+// Use the Box<dyn Error> trait object to get a
 // type impls `Error`
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    
+
     let results = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
     };
-    
+
     for line in results {
         println!("{}", line);
     }
-    
+
     Ok(())
 }
 
@@ -87,7 +87,7 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents.lines()
         .filter(|line| line.contains(&query))
         .collect()
-} 
+}
 
 /*Search case insentive in the file*/
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -137,7 +137,6 @@ Trust me";
             vec!["Rust,", "Trust me"],
             search_case_insensitive(query, contents)
         );
-        
+
     }
 }
-
